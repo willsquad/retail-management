b123479a689bff2823392379c9ab554a52f0310c
Ashil John <willsquad.aj@gmail.com>
2018-10-04 15:40:13 +0530

    Add the sales.php page with the icons
    This commit will add the sales.php page with the icons.

--- sales.php
+++ sales.php
@@ -14,7 +14,96 @@
         <div class="manager_rhs__content">
             <h5 class="manager_rhs__content__title">Sale</h5>
 
-            
+            <!-- RHS Content: Body -->
+            <div class="manager_rhs__content__body">
+                <!-- Icon container -->
+                <div class="icon_container sales">
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="register.php">
+                        <i class="fas fa-reply icon_flip"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">Continue Sale</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->        
+
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="register.php">
+                        <i class="fas fa-star"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">Special Order</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->        
+
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="register.php">
+                        <i class="fas fa-umbrella"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">Layaway</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->        
+
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="register.php">
+                        <i class="fas fa-ticket-alt"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">Refund</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->        
+
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="register.php">
+                        <i class="fas fa-plus"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">New Sale</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->        
+                </div>
+                <!-- Icon container -->
+            </div>
+            <!-- RHS Content: Body -->
+
+
+            <h5 class="manager_rhs__content__title">Register</h5>
+
+            <!-- RHS Content: Body -->
+            <div class="manager_rhs__content__body">
+                <!-- Icon container -->
+                <div class="icon_container sales">
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="register.php">
+                        <i class="fas fa-power-off"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">Close Register</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->        
+
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="register.php">
+                        <i class="fas fa-minus-circle"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">Payout / Drop</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->        
+
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="register.php">
+                        <i class="fas fa-plus-circle"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">Plus Circle</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->             
+                </div>
+                <!-- Icon container -->
+            </div>
+            <!-- RHS Content: Body -->
         </div>
         <!-- Manager RHS: Content -->
     </div>
