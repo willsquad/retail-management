ec3bbcefa3ced2c86a93fc19b951ac6d43b637db
Ashil John <willsquad.aj@gmail.com>
2018-10-12 12:14:01 +0530

    Add the customers.php page with customer list, credit accounts and types

---
+++ customers.php
@@ -0,0 +1,58 @@
+<?php include('include/header.php');?>
+
+<!-- START OF RHS  -->
+    <!-- Manager RHS -->
+    <div class=" manager_rhs animated fadeIn">
+        <!-- Manager RHS: Top Bar -->
+        <div class="manager_rhs__top_bar">
+            <div class="rhs_menu_icon"><i class="fas fa-bars"></i></div>
+            <div class="rhs_title">Customers</div>
+        </div>
+        <!-- Manager RHS: Top Bar -->
+
+        <!-- Manager RHS: Content -->
+        <div class="manager_rhs__content">
+
+            <!-- New Section -->
+            <h5 class="manager_rhs__content__title">Customers</h5>
+            <!-- RHS Content: Body -->
+            <div class="manager_rhs__content__body">
+                <!-- Icon container -->
+                <div class="icon_container sales">
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="customer-list.php">
+                        <i class="fas fa-user"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">Customers</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->        
+
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="new-item.php">
+                        <i class="fas fa-user-clock"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">Credit Accounts</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->        
+
+                    <!-- Icon container link-->
+                    <a class="icon_a" href="register.php">
+                        <i class="fas fa-users"></i>
+                        <div class="icon_text">
+                            <span class="icon_name">Customer Types</span>
+                        </div>
+                    </a>  
+                    <!-- Icon container link-->             
+                </div>
+                <!-- Icon container -->
+            </div>
+            <!-- RHS Content: Body -->
+        </div>
+        <!-- Manager RHS: Content -->
+    </div>
+    <!-- Manager RHS -->
+<!-- END OF RHS -->
+
+<?php include('include/footer.php');?>
