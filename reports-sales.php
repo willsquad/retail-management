6a81522f477c9819a386a1d88a1b49cd615d2dbf
Ashil John <willsquad.aj@gmail.com>
2018-10-15 13:01:53 +0530

    Add & modify the search filter section for the sales report page

--- reports-sales.php
+++ reports-sales.php
@@ -18,9 +18,15 @@
             <div class="manager_rhs__content__body">
                  <!-- Search Item -->
                 <div class="search_items_container">
-                    <input type="text" class="search_items search_report_date" placeholder="Search By Date (From)" onfocus="(this.type='date')" onblur="(this.type='text')">
-                    <input type="search" class="search_items" placeholder="Search By ID">
-                    <button class="search_item_button">Search</button>
+                    <div class="search_items_div">
+                        <input type="text" class="search_items search_report_date" placeholder="Search By Date (From)" onfocus="(this.type='date')" onblur="(this.type='text')" onblur="(this.value='2018-10-15')">
+                        <input type="text" class="search_items search_report_date" placeholder="Search By Date (To)" onfocus="(this.type='date')" onblur="(this.type='text')" onblur="(this.value='2018-10-15')">
+                        <input type="search" class="search_items" placeholder="Search By ID">
+                        <button class="search_item_button">Search</button>
+                    </div>
+                    <div class="search_items_advanced">
+                        Advanced <i class="fas fa-chevron-down"></i>
+                    </div>
                 </div>
                 <!-- Search Item -->
 
