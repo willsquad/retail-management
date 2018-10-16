7b7b9c731fec45ca0fa06729de54e3ed7ff8af04
Ashil John <willsquad.aj@gmail.com>
2018-10-16 14:36:20 +0530

    Modify the reports.php page to wrap the sections in report_sections_div

--- reports.php
+++ reports.php
@@ -17,87 +17,95 @@
                 <button class="reporting_nav_button advanced_report_button">Advanced Reporting</button>
             </div>      
 
-            <h5 class="manager_rhs__content__title">Sales Report</h5>
-            <!-- RHS Content: Body -->
-            <div class="manager_rhs__content__body">
-                <!-- Icon container -->
-                <div class="icon_container sales">
-                    <!-- Icon container link-->
-                    <a class="icon_a" href="new-sale.php">
-                        <i class="fas fa-shopping-cart"></i>
-                        <div class="icon_text">
-                            <span class="icon_name">All Sales</span>
-                        </div>
-                    </a>  
-                    <!-- Icon container link-->            
-                    <!-- Icon container link-->
-                    <a class="icon_a" href="new-sale.php">
-                        <i class="fas fa-shopping-bag"></i>
-                        <div class="icon_text">
-                            <span class="icon_name">Unit Sales</span>
-                        </div>
-                    </a>  
-                    <!-- Icon container link-->            
+            <div class="report_sections_div">
+                <h5 class="manager_rhs__content__title reports_title">Sales Report</h5>
+                <!-- RHS Content: Body -->
+                <div class="manager_rhs__content__body">
+                    <!-- Icon container -->
+                    <div class="icon_container sales">
+                        <!-- Icon container link-->
+                        <a class="icon_a" href="reports-sales.php">
+                            <i class="fas fa-shopping-cart"></i>
+                            <div class="icon_text">
+                                <span class="icon_name">All Sales</span>
+                            </div>
+                        </a>  
+                        <!-- Icon container link-->            
+                        <!-- Icon container link-->
+                        <a class="icon_a" href="unit-sales-report.php">
+                            <i class="fas fa-shopping-bag"></i>
+                            <div class="icon_text">
+                                <span class="icon_name">Unit Sales</span>
+                            </div>
+                        </a>  
+                        <!-- Icon container link-->            
+                    </div>
+                    <!-- Icon container -->
                 </div>
-                <!-- Icon container -->
+                <!-- RHS Content: Body -->    
             </div>
-            <!-- RHS Content: Body -->    
+            
 
-
-            <h5 class="manager_rhs__content__title">Transactions Report</h5>
-            <!-- RHS Content: Body -->
-            <div class="manager_rhs__content__body">
-                <!-- Icon container -->
-                <div class="icon_container sales">
-                    <!-- Icon container link-->
-                    <a class="icon_a" href="new-sale.php">
-                        <i class="fas fa-dollar-sign"></i>
-                        <div class="icon_text">
-                            <span class="icon_name">All Transactions</span>
-                        </div>
-                    </a>  
-                    <!-- Icon container link-->                   
+            <div class="report_sections_div">
+                <h5 class="manager_rhs__content__title reports_title">Transactions Report</h5>
+                <!-- RHS Content: Body -->
+                <div class="manager_rhs__content__body">
+                    <!-- Icon container -->
+                    <div class="icon_container sales">
+                        <!-- Icon container link-->
+                        <a class="icon_a" href="all-transactions-report.php">
+                            <i class="fas fa-dollar-sign"></i>
+                            <div class="icon_text">
+                                <span class="icon_name">All Transactions</span>
+                            </div>
+                        </a>  
+                        <!-- Icon container link-->                   
+                    </div>
+                    <!-- Icon container -->
                 </div>
-                <!-- Icon container -->
+                <!-- RHS Content: Body -->   
             </div>
-            <!-- RHS Content: Body -->   
 
-
-            <h5 class="manager_rhs__content__title">Inventory Reports</h5>
-            <!-- RHS Content: Body -->
-            <div class="manager_rhs__content__body">
-                <!-- Icon container -->
-                <div class="icon_container sales">
-                    <!-- Icon container link-->
-                    <a class="icon_a" href="inventory-reports.php">
-                        <i class="fas fa-truck"></i>
-                        <div class="icon_text">
-                            <span class="icon_name">Current Inventory</span>
-                        </div>
-                    </a>  
-                    <!-- Icon container link-->                   
+            
+            <div class="report_sections_div">
+                <h5 class="manager_rhs__content__title reports_title">Inventory Reports</h5>
+                <!-- RHS Content: Body -->
+                <div class="manager_rhs__content__body">
+                    <!-- Icon container -->
+                    <div class="icon_container sales">
+                        <!-- Icon container link-->
+                        <a class="icon_a" href="inventory-reports.php">
+                            <i class="fas fa-truck"></i>
+                            <div class="icon_text">
+                                <span class="icon_name">Current Inventory</span>
+                            </div>
+                        </a>  
+                        <!-- Icon container link-->                   
+                    </div>
+                    <!-- Icon container -->
                 </div>
-                <!-- Icon container -->
+                <!-- RHS Content: Body -->   
             </div>
-            <!-- RHS Content: Body -->   
 
-            <h5 class="manager_rhs__content__title">Register Reports</h5>
-            <!-- RHS Content: Body -->
-            <div class="manager_rhs__content__body">
-                <!-- Icon container -->
-                <div class="icon_container sales">
-                    <!-- Icon container link-->
-                    <a class="icon_a" href="closing-register-reports.php">
-                        <i class="fas fa-inbox"></i>
-                        <div class="icon_text">
-                            <span class="icon_name">Closing Register</span>
-                        </div>
-                    </a>  
-                    <!-- Icon container link-->                   
+            <div class="report_sections_div">
+                <h5 class="manager_rhs__content__title reports_title">Register Reports</h5>
+                <!-- RHS Content: Body -->
+                <div class="manager_rhs__content__body">
+                    <!-- Icon container -->
+                    <div class="icon_container sales">
+                        <!-- Icon container link-->
+                        <a class="icon_a" href="closing-register-reports.php">
+                            <i class="fas fa-inbox"></i>
+                            <div class="icon_text">
+                                <span class="icon_name">Closing Register</span>
+                            </div>
+                        </a>  
+                        <!-- Icon container link-->                   
+                    </div>
+                    <!-- Icon container -->
                 </div>
-                <!-- Icon container -->
+                <!-- RHS Content: Body -->   
             </div>
-            <!-- RHS Content: Body -->   
 
         </div>
         <!-- Manager RHS: Content -->
